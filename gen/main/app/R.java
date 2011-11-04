503dac1311589d9c69dd548c041ef16ef1c0d630
Matthew Ito <mito81@gmail.com>
2011-11-04 14:02:25 -0700

    Added videooverlay.xml

--- gen/main/app/R.java
+++ gen/main/app/R.java
@@ -25,20 +25,25 @@ public final class R {
         public static final int homeButton=0x7f050002;
         public static final int imageView1=0x7f05000a;
         public static final int logoImage=0x7f050001;
+        public static final int overlayButton=0x7f05000f;
         public static final int poweredByText=0x7f050009;
         public static final int progressBar1=0x7f05000b;
         public static final int randomButton=0x7f050005;
+        public static final int relativeLayout1=0x7f05000c;
+        public static final int scrollView1=0x7f05000e;
         public static final int testText=0x7f050007;
         public static final int textTitle=0x7f050008;
         public static final int topBar=0x7f050000;
         public static final int uploadButton=0x7f050004;
         public static final int videoButton=0x7f050003;
+        public static final int videoView1=0x7f05000d;
         public static final int webview=0x7f050006;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int random=0x7f030001;
         public static final int splash=0x7f030002;
+        public static final int videooverlay=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
